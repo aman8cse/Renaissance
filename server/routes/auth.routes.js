6b6eed498245178c4d115314ab1e24818d7ac968
Vardan Singhal <vardansinghal3808@gmail.com>
2025-11-24 02:09:01 +0530

    setup user auth routes

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { registerUser, loginUser, refreshAccessToken, logoutUser } from "../controllers/authController.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/refresh-token", refreshAccessToken);
+router.post("/logout", logoutUser);
+
+// protected route
+router.get("/me", verifyToken, (req, res) => {
+  res.status(200).json({ message: "Authenticated", user: req.user });
+});
+
+export default router;
